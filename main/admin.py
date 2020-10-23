0438b2481c1b0aa2bf8f402240f102573d5b3277
Sheikh Ahaduzzaman <ahadsheikh1999@gmail.com>
2020-10-23 18:03:56 +0600

    Compound model working

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from main.models import Compound
 
-# Register your models here.
+
+admin.site.register(Compound)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Compound(models.Model):
+    PID = models.CharField(max_length=60)
+    Smiles = models.CharField(max_length=250)
+    Molecular_Formula = models.CharField(max_length=50)
+    Molecular_Weight = models.FloatField()
+    H_Bond_Acceptors = models.IntegerField()
+    H_Bond_Donors = models.IntegerField()
+    Molar_Refractivity = models.FloatField()
+    TPSA = models.FloatField()
+    ROMol = models.TextField(max_length=150000)
+
+    def __str__(self):
+        return self.Molecular_Formula
