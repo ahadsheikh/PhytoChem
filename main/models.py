2b048d9ffaa3fd4ec2cb85275b8d015d8bef9e64
Sheikh Ahaduzzaman <ahadsheikh1999@gmail.com>
2020-10-27 13:39:08 +0600

    about

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Compound(models.Model):
     PID = models.CharField(max_length=60, unique=True)
-    Smiles = models.CharField(max_length=250, db_index=True)
+    Smiles = models.CharField(max_length=250, db_index=True, unique=True)
     Molecular_Formula = models.CharField(max_length=50, db_index=True)
     Molecular_Weight = models.FloatField()
     H_Bond_Acceptors = models.IntegerField()
@@ -14,3 +14,4 @@ class Compound(models.Model):
 
     def __str__(self):
         return self.Molecular_Formula
+
