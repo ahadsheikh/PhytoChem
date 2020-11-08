676aff9938a62aea03e951a5934b013d2f4a1361
Sheikh Ahaduzzaman <ahadsheikh1999@gmail.com>
2020-11-08 13:25:40 +0600

    compound and plant details view

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('results/', views.results, name='results'),
-    path('download/', views.download_file, name='download_all_results')
+    path('download/', views.download_file, name='download_all_results'),
+
+
+    # Test
+    path('main/plant/<int:id>/', views.plant, name='plant'),
+    path('main/compound/<int:id>/', views.compound, name='compound'),
 ]
