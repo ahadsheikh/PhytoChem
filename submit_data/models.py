1d52a955984a193d7762362448f5c6a59690bee5
Sheikh Ahaduzzaman <ahadsheikh1999@gmail.com>
2020-11-17 13:07:18 +0600

    added ContributorDatabase model

--- submit_data/models.py
+++ submit_data/models.py
@@ -11,3 +11,13 @@ class Contributor(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ContibutorDatabase(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
