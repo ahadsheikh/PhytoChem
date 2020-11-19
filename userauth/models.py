6409593021f8597574588d923aefcd61fb2ccb97
Sheikh Ahaduzzaman <ahadsheikh1999@gmail.com>
2020-11-19 23:39:04 +0600

    create user createn signal

--- userauth/models.py
+++ userauth/models.py
@@ -1,7 +1,16 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     about = models.TextField(max_length=250, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_profile_signal(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
