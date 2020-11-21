dd112f7d61dfcb79abe55344bb91c8b0e05f039e
Shafquat Mahbub <mahbub.shafquat@gmail.com>
2020-11-21 18:35:33 +0600

    Added user to Contributor

--- submit_data/models.py
+++ submit_data/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Contribution(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     plant_name = models.CharField(max_length=100)
     pub_link = models.URLField(max_length=200)
     data_description = models.TextField(max_length=500, blank=True)
