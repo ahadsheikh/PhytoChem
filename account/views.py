9a2c7db89e55923c3066173cc350d687d13b60a8
Sheikh Ahaduzzaman <ahadsheikh1999@gmail.com>
2020-12-13 12:21:15 +0600

    code fix

--- account/views.py
+++ account/views.py
@@ -1,9 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages, auth
 
